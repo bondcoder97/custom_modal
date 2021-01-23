316557a3297182d08a6ce1a695e83cc91f56ac41
Olga Khinevich <olga-khinevich@yandex.ru>
2021-01-24 00:21:30 +0300

    changes

--- confirm_modal.js
+++ confirm_modal.js
@@ -1,26 +1,41 @@
 (function(){
-    function confirm_modal(text, options={}){
+    async function confirm_modal(text, options={}){
         const {modal_type="default", header="Attention!"} = options;
         if(!text) throw new Error("Modal text is required!");
         const modal_wrapper = document.createElement("div");
-        modal_wrapper.className = "confirm_modal_wrapper";
+        modal_wrapper.className = "custom_modal_wrapper";
         modal_wrapper.innerHTML = `
-            <div class="confirm_modal ${modal_type !== "default"? modal_type : ""}">
-                <div class="confirm_modal_header">
+            <div class="custom_modal ${modal_type !== "default"? modal_type : ""}">
+                <div class="custom_modal_header">
                     ${header}
                 </div>
-                <div class="confirm_modal_content">
+                <div class="custom_modal_content">
                     <p>${text}</p>
                 </div>
-                <div class="confirm_modal_btns">
+                <div class="custom_modal_btns">
                     <span class="accept_btn btn">OK</span>
                     <span class="cancel_btn btn">CANCEL</span>
                 </div>
             </div>
         `;
-        modal_wrapper.querySelector(".accept_btn").addEventListener("click", ()=> modal_wrapper.remove());
-        modal_wrapper.querySelector(".cancel_btn").addEventListener("click", ()=> modal_wrapper.remove());
+        
+        modal_wrapper.querySelector(".accept_btn").addEventListener("click", ()=> {
+            dispatchEvent(new CustomEvent("user_answer", {detail: {answer: true}}))
+            modal_wrapper.remove()
+        })
+        modal_wrapper.querySelector(".cancel_btn").addEventListener("click", ()=> {
+            dispatchEvent(new CustomEvent("user_answer", {detail: {answer: false}}))
+            modal_wrapper.remove()
+        })
         document.body.appendChild(modal_wrapper);
+
+       // объект promise
+        const answer = await new Promise((resolve, reject) => {
+            modal_wrapper.addEventListener("user_answer", function(data) {
+                resolve(data.detail.answer)
+            })
+        });
+        return answer;
     }
 
     window.confirm_modal = confirm_modal;
