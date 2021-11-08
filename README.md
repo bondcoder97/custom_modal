a8218e3c384332fe9b56c7081aee197cd953ce8c
bondcoder <andruha3097@gmail.com>
2021-11-08 15:26:09 +0300

    new: commonjs and edited readme

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 Simple modals instead native "alert" that often can be blocked by browser. 
 
-# Modal types
+## Modal types
 - success
 - error
 - default
 
-# HTML file
-<script src="alert_modal.js">  </script>
-<script src="confirm_modal.js">  </script>
+## 1. Include styles
+CSS: <b>/style/css/custom_modal.css</b>  
+SCSS: <b>/style/scss/custom_modal.scss</b>
 
-# Scss
-@import "./custom_modal";
+## 2. Include Scripts
 
-# Use
-alert_modal("my text")
-await confirm_modal("Are you really want to do it?")
+### Browser environment
+alert_modal.js  
+confirm_modal.js  
+
+### CommonJS environment
+import {alert_modal, confirm_modal} from "modals.js"; 
+
+
+## Usage
+
+alert_modal("my text")  
+await confirm_modal("Are you really want to do it?")  
+alert_modal("Error: Something went wrong!", {modal_type: "error"})
---
+++ modals.js
@@ -0,0 +1,63 @@
+function alert_modal(text, options={}){
+    const {modal_type="default", header="Attention!"} = options;
+    if(!text) throw new Error("Modal text is required!");
+    const modal_wrapper = document.createElement("div");
+    modal_wrapper.className = "custom_modal_wrapper";
+    modal_wrapper.innerHTML = `
+        <div class="custom_modal ${modal_type !== "default"? modal_type : ""}">
+            <div class="custom_modal_header">
+                ${header}
+            </div>
+            <div class="custom_modal_content">
+                <p>${text}</p>
+            </div>
+            <div class="custom_modal_btns">
+                <span class="accept_btn btn">OK</span>
+            </div>
+        </div>
+    `;
+    modal_wrapper.querySelector(".accept_btn").addEventListener("click", ()=> modal_wrapper.remove());
+    document.body.appendChild(modal_wrapper);
+}
+
+async function confirm_modal(text, options={}){
+    const {modal_type="default", header="Attention!"} = options;
+    if(!text) throw new Error("Modal text is required!");
+    const modal_wrapper = document.createElement("div");
+    modal_wrapper.className = "custom_modal_wrapper";
+    modal_wrapper.innerHTML = `
+        <div class="custom_modal ${modal_type !== "default"? modal_type : ""}">
+            <div class="custom_modal_header">
+                ${header}
+            </div>
+            <div class="custom_modal_content">
+                <p>${text}</p>
+            </div>
+            <div class="custom_modal_btns">
+                <span class="accept_btn btn">OK</span>
+                <span class="cancel_btn btn">CANCEL</span>
+            </div>
+        </div>
+    `;
+    
+    modal_wrapper.querySelector(".accept_btn").addEventListener("click", ()=> {
+        modal_wrapper.dispatchEvent(new CustomEvent("user_answer", {detail: {answer: true}}));
+    });
+
+    modal_wrapper.querySelector(".cancel_btn").addEventListener("click", ()=> {
+        modal_wrapper.dispatchEvent(new CustomEvent("user_answer", {detail: {answer: false}}));
+    });
+    document.body.appendChild(modal_wrapper);
+
+    const answer = await new Promise((resolve, reject) => {
+        modal_wrapper.addEventListener("user_answer", function({detail: {answer}}) {
+            resolve(answer);
+            modal_wrapper.remove();
+        })
+    });
+    return answer;
+}
+
+
+module.exports.alert_modal = alert_modal;
+module.exports.confirm_modal = confirm_modal;
